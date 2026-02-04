c0342a6831a1f863698c3f721fb8a93063594f84
rtaylor456 <ruth.eloisa.456@gmail.com>
2026-02-03 23:27:47 -0700

    update simulated scores data (rda file version) example

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,7 +5,7 @@
 rsa_simulated <- read.csv("data-raw/rsa_simulated_subset_fixed.csv")
 
 
-scores_simulated <- read.csv("data-raw/scores_simulated_subset.csv")
+scores_simulated <- read.csv("data-raw/scores_simulated_fixed.csv")
 
 
 usethis::use_data(rsa_simulated, overwrite = TRUE)
